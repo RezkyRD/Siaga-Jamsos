366cd4a568011bb599bd9fe273626946a6e73e25
RezkyRD <rezkysurya678@gmail.com>
2026-03-02 22:54:56 +0700

    add scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import os
 
 def run_scraper():
-
     rss_sources = {
         "CNN": "https://www.cnnindonesia.com/nasional/rss",
         "Tribunnews": "https://www.tribunnews.com/rss",
@@ -14,14 +13,12 @@ def run_scraper():
     }
 
     all_news = []
-    today = datetime.utcnow().date()   # fallback jika RSS tidak punya tanggal
+    today = datetime.utcnow().date()
 
     for media, url in rss_sources.items():
         feed = feedparser.parse(url)
 
         for entry in feed.entries:
-
-            # ambil tanggal dari RSS
             published = entry.get("published_parsed")
 
             if published:
@@ -39,10 +36,8 @@ def run_scraper():
             })
 
     df = pd.DataFrame(all_news)
-
     file_name = "raw_news.csv"
 
-    # append mode
     if os.path.exists(file_name):
         old_data = pd.read_csv(file_name)
         combined = pd.concat([old_data, df], ignore_index=True)
@@ -51,9 +46,7 @@ def run_scraper():
         combined = df
 
     combined.to_csv(file_name, index=False)
-
     print("Scraping selesai. Data ditambahkan ke raw_news.csv")
-
 
 if __name__ == "__main__":
     run_scraper()
